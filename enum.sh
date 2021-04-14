1b46c4226025e61cc29840202bdd45555a32ea54
peixetlift <74323383+peixetlift@users.noreply.github.com>
2021-04-14 16:07:47 +0200

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -2,18 +2,22 @@
 
 echo "What is the machine's @IP?"
 read IP
-echo "The port scan is loading... check ports.txt when finished!"
-nmap -A -T5 $IP > ports.txt
-echo "Do you wish to run a directory scan?(y/n)"
+echo  -e "The port scan is loading... check ports.txt when finished!\n"
+nmap -A -T5 $IP >ports.txt
+echo "OPEN PORTS :"
+echo "--------------------------"
+while read -r line; do
+	if [[ $line = *[0-9]"/"* ]]; then
+		echo "$line" | awk '{print substr($line,0,20)}'
+	fi
+done <ports.txt
+echo -e "--------------------------\n"
+echo -e "Do you wish to run a directory scan?(y/n)\n"
 read ans
-y="y"
-n="n"
-if [ "$ans" = "$y" ] 
-then
+if [ "$ans" = "y" ]; then
 	echo "The directory scan is loading... check directories.txt when finished!"
 	gobuster dir -u $IP -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt | tee directories.txt
-elif [ "$ans" = "$n" ] 
-then
+elif [ "$ans" = "n" ]; then
 	echo "Bye!"
 else
 	echo "Wrong input!"
